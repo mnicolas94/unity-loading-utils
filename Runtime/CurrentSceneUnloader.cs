e51bc6a355bf425a97a8fe291a28ff545f4b8ef5
mnicolas94 <miguelcok27@gmail.com>
2023-11-17 11:13:24 -0500

    feat: implement CurrentSceneUnloader class

---
+++ Runtime/CurrentSceneUnloader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LoadingUtils
+{
+    public class CurrentSceneUnloader : MonoBehaviour
+    {
+        public void Unload()
+        {
+            var currentScene = SceneManager.GetActiveScene();
+            SceneManager.UnloadSceneAsync(currentScene.name);
+        }
+    }
+}
