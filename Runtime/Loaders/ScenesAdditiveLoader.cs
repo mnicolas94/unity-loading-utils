a75502b46f891f831431e294b88475228c2e153c
mnicolas94 <miguelcok27@gmail.com>
2023-11-17 10:52:02 -0500

    feat: implement ScenesAdditiveLoader

---
+++ Runtime/Loaders/ScenesAdditiveLoader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AsyncUtils;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using ZeShmouttsAssets.DataContainers;
+
+namespace LoadingUtils.Loaders
+{
+    [Serializable]
+    public class ScenesAdditiveLoader : ILoader
+    {
+        [SerializeField] private List<SceneData> _scenes;
+
+        public async Task Load(CancellationToken ct)
+        {
+            foreach (var scene in _scenes)
+            {
+                await SceneManager.LoadSceneAsync(scene.name).AwaitAsync(ct);
+            }
+        }
+    }
+}
